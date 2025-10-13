b6c83482e22058bf52bc181be6d853692ff73123
Ny-Antsaniaina <hei.nyantsaniaina@gmail.com>
2025-10-13 19:23:08 +0300

    add project spring boot

---
+++ src/main/java/org/example/projectface/ProjectFaceApplication.java
@@ -0,0 +1,13 @@
+package org.example.projectface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/projectface/ProjectFaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projectface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
